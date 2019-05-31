c9a384eeafefae74f1494208034a33bfb4880a9e
seu nome <youremail@domain.com>
2019-05-31 01:25:24 -0300

    origin to all get

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 const port = require('./app/config/port')
 const path = require('path');
 
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/public/turtle.html'));
@@ -11,6 +15,7 @@ app.get('/',function(req,res){
 });
 
 app.use(express.static(__dirname + 'public'));
+
 
 app.use('/public', express.static('public'));
 
