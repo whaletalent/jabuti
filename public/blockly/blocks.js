3feccfb353de7c64f58ea7a69099de41433eaab4
seu nome <youremail@domain.com>
2019-06-01 13:58:46 -0300

    added new blockls

--- public/blockly/blocks.js
+++ public/blockly/blocks.js
@@ -62,6 +62,35 @@ Blockly.Blocks['color'] = {
     this.setHelpUrl('');
   }
 };
+Blockly.Blocks['pdv'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldImage("/public/assets/pd.png", 26, 26, "*"));
+    this.appendValueInput("grau")
+        .setCheck("Number");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(290);
+    this.setTooltip('Gira-o para direita (Aceita blocos e variáveis)');
+    this.setHelpUrl('');
+  }
+};
+Blockly.Blocks['pev'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldImage("/public/assets/pe.png", 26, 26, "*"));
+    this.appendValueInput("grau")
+        .setCheck("Number");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(290);
+    this.setTooltip('Gira-o para esquerda (Aceita blocos e variáveis)');
+    this.setHelpUrl('');
+  }
+};
+
 
 //JS INJECT
 Blockly.JavaScript['pf'] = function(block) {
@@ -90,5 +119,18 @@ Blockly.JavaScript['pe'] = function(block) {
 Blockly.JavaScript['color'] = function(block) {
   var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
   var code = 'setColor('+value_name+');\n';
+  return code;
+};
+
+Blockly.JavaScript['pdv'] = function(block) {
+  var value_grau = Blockly.JavaScript.valueToCode(block, 'grau', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'jabutiPD('+value_grau+');\n';
+  return code;
+};
+Blockly.JavaScript['pev'] = function(block) {
+  var value_grau = Blockly.JavaScript.valueToCode(block, 'grau', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'jabutiPE('+value_grau+');\n';
   return code;
 };
